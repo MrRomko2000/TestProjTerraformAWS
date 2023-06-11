a6643acbe5d7e60667db9ea89bedc4473518107b
Roman Knysh <rknysh.1990@gmail.com>
2023-06-11 21:48:37 +0000

    added VPC

---
+++ installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
+
+
+
+#bash installation.sh
